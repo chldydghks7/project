ebc103914c7877d61cd7e0f904e11edace9c902c
shawn1574 <shawn1574@naver.com>
2017-07-05 15:10:46 +0900

    지운이후
    ㅑㅐㅑㅐ

--- GroupRiding/src/main/java/group/riding/service/RaceService.java
+++
@@ -1,8 +0,0 @@
-package group.riding.service;
-
-public interface RaceService {
-
-	
-    public void insertRaceRecord(String racepoint_time)throws Exception;//일단 gr_id 그리고 거점 아이디 고정으로 들어감 나중에 변경
-    
-}
